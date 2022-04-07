a3db54a21285e855d062fb8fe758900b5f47a383
valean27 <stefan.valean27@gmail.com>
2022-04-07 19:40:01 +0300

    Am adaugat o gramada

--- src/main/resources/static/main.js
+++
@@ -1,9 +0,0 @@
-Date.prototype.toDateInputValue = (function() {
-    var local = new Date(this);
-    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
-    return local.toJSON().slice(0,10);
-});
-$( document ).ready(function() {
-    console.log( "ready!" );
-    document.getElementById('date').value = new Date().toDateInputValue();
-});
