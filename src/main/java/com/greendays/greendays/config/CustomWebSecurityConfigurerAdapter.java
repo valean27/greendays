ccf9d19e401423d6b7383ee0f534dc7ec5cc7d18
bfilip <bogdore23@gmail.com>
2022-04-08 16:49:33 +0300

    h2 was not working issue

--- src/main/java/com/greendays/greendays/config/CustomWebSecurityConfigurerAdapter.java
+++ src/main/java/com/greendays/greendays/config/CustomWebSecurityConfigurerAdapter.java
@@ -4,6 +4,7 @@ package com.greendays.greendays.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -37,5 +38,13 @@ public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAda
                         .roles("ADMIN")
                         .build()
         );
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**");
     }
 }
