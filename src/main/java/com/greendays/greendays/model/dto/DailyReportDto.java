75b970e314f07a6a84889dc8da68e589033737ff
bfilip <bogdore23@gmail.com>
2023-02-10 14:34:55 +0200

    - revert modification

--- src/main/java/com/greendays/greendays/model/dto/DailyReportDto.java
+++ src/main/java/com/greendays/greendays/model/dto/DailyReportDto.java
@@ -4,6 +4,7 @@ import com.greendays.greendays.model.dal.Client;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.math.BigDecimal;
 import java.sql.Date;
@@ -11,6 +12,7 @@ import java.sql.Date;
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 public class DailyReportDto {
     private Long id;
     private Date date;
